bb420b4f17719defd5d6028b74521a0d2e6f03ff
Patrick Halim <pnhalim@umich.edu>
2022-12-28 00:12:15 -0500

    finish front page

---
+++ main.js
@@ -0,0 +1,14 @@
+const hi = document.getElementById("hi");
+
+const mq = matchMedia( '( min-width: 700px )' );
+const updateViewBox = () => {
+  if (mq.matches) {
+    hi.setAttribute( 'viewBox', "200 0 1600 400" );
+  } else {
+    hi.setAttribute( 'viewBox', "400 0 800 400" );
+  }
+}
+hi.addEventListener( 'SVGLoad', updateViewBox );
+mq.addEventListener( 'change', updateViewBox );
+
+
