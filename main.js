012c2f72d577ed86b99c2291f8aef5e584b947e0
Patrick Halim <pnhalim@umich.edu>
2023-03-07 18:54:43 -0500

    restarting with new designs. fix hi

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const hi = document.getElementById("hi-svg");
+const hi = document.getElementById("start-hi-svg");
 
 const mq = matchMedia( '( min-width: 700px )' );
 const updateViewBox = () => {
   if (mq.matches) {
-    hi.setAttribute( 'viewBox', "200 0 1600 400" );
+    hi.setAttribute( 'viewBox', "300 0 1200 400" );
   } else {
-    hi.setAttribute( 'viewBox', "400 0 800 400" );
+    hi.setAttribute( 'viewBox', "400 0 400 400" );
   }
 }
 hi.addEventListener( 'SVGLoad', updateViewBox );
