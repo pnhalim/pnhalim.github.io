75c02a1745f6ee76722085ca89153f3836ff5dd2
pnhalim <pnhalim@umich.edu>
2023-06-15 00:44:12 -0400

    Fix mobile scrolling

--- src/loco.js
+++ src/loco.js
@@ -3,5 +3,14 @@ import LocomotiveScroll from 'locomotive-scroll';
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true, 
-    multiplier: 0.5
+    multiplier: 0.5,
+    reloadOnContextChange: true, 
+    smartphone: {
+        smooth: true
+    },
+    tablet: {
+        smooth: true
+    }
 });
+
+setTimeout(function(){ scroll.update(); console.log('update scroller') }, 5000);
