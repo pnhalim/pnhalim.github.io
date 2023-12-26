e1d1e5849f63badffa5425f5eac4eaab139f4d1d
pnhalim <pnhalim@umich.edu>
2023-12-26 11:08:52 -0500

    add search-engine project page

---
+++ docs/index.47fa4176.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".mobile-menu");e.querySelector(".nav-tgl").addEventListener("click",t=>{e.classList.toggle("active")});
+//# sourceMappingURL=index.47fa4176.js.map
--- docs/index.6fbc508a.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".mobile-menu"),t=e.querySelector(".nav-tgl");t.addEventListener("click",t=>{e.classList.toggle("active")});
-//# sourceMappingURL=index.6fbc508a.js.map
